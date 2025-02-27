e56cb80d708561f51d81c89ed7a10698b53ef816
gift95 <55743744+gift95@users.noreply.github.com>
2025-02-27 11:04:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+# entrypoint.sh
 
-set -x
-if [ ! -f /opt/tg-signer/start.sh ]; then \
-    echo "#!/bin/bash" > /opt/tg-signer/start.sh; \
-    echo "echo 'start.sh not found, creating default one'" >> /opt/tg-signer/start.sh; \
-    chmod +x /opt/tg-signer/start.sh; \
+# 检查 /opt/tg-signer/start.sh 文件是否存在
+if [ ! -f /opt/tg-signer/start.sh ]; then
+  echo "start.sh 文件不存在，正在创建默认的 start.sh 文件..."
+  
+  # 创建 start.sh 文件
+    cp  /opt/tg-signer-config/start.sh  /opt/tg-signer/start.sh
+  # 给 start.sh 文件执行权限
+  chmod +x /opt/tg-signer/start.sh
 fi
+
+# 执行 start.sh 文件
+echo "正在执行 start.sh 文件..."
+exec /opt/tg-signer/start.sh "$@"
